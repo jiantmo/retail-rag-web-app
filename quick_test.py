b9ab56ba1bb00b25ee348b31af295d87946255da
Steve Mo <jiantmo@microsoft.com>
2025-08-14 18:15:00 -0700

    update

--- quick_test.py
+++
@@ -1,4 +0,0 @@
-from generate_test_cases import TestCaseGenerator
-gen = TestCaseGenerator('AdventureWorksDemoDataProducts_with_embedding.json', 'test_case')
-result = gen._determine_category('Camping Tent', 'Waterproof tent for outdoor camping')
-print('Camping Tent:', result)
